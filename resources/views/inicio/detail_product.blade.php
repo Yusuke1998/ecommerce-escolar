064348af09583d45324eb6124732f4e7389b1422
jhonnyprz <jhonnyjose1998ii@gmail.com>
2019-07-02 01:20:57 -0400

    ajustes

--- resources/views/inicio/detail_product.blade.php
+++ resources/views/inicio/detail_product.blade.php
@@ -3,22 +3,26 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div id="gallery" class="span3">
+		<div id="gallery" class="span4">
+			@if($producto->images->count() > 0)
 		    <a href="{{ asset('img/productos').'/'.$producto->images->first()->name }}" title="Fujifilm FinePix S2950 Digital Camera') }}">
 
 				<img src="{{ asset('img/productos').'/'.$producto->images->first()->name }}" style="width:90%" alt="{{ $producto->name }}"/>
 		    </a>
+		    @endif
 			<div id="differentview" class="moreOptopm carousel slide">
 		        <div class="carousel-inner">
 		          <div class="item active">
-		          	@foreach($producto->images->take(4) as $imagen)
+		          @forelse($producto->images->take(4) as $imagen)
 		           <a href="{{ asset('img/productos').'/'.$imagen->name }}"> <img style="width:18%" src="{{ asset('img/productos').'/'.$imagen->name }}" alt=""/></a>
-		           @endforeach
+		           @empty
+		           	<p align="center">No hay imagenes para mostrar</p>
+		           @endforelse
 		        </div>
 		    </div>
 		</div>
 
-		<div class="span4">
+		<div class="span8">
 			<h3>{{ $producto->name }}</h3>
 				<hr class="soft"/>
 				<form class="form-horizontal qtyFrm">
