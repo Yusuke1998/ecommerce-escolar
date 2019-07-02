1cf8c3a4b49e88ad29aa1063a47b99fecf63aea9
jhonnyprz <jhonnyjose1998ii@gmail.com>
2019-07-02 12:29:31 -0400

    quitando link viejo :v

--- resources/views/inicio/sidebar.blade.php
+++ resources/views/inicio/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Sidebar ================================================== -->
 	<div id="sidebar" class="span3">
 		@auth
-		<div class="well well-small"><a id="myCart" href="product_summary.html"><img src="{{ asset('plantilla-ecommerce/themes/images/ico-cart.png') }}" alt="cart">
+		<div class="well well-small"><a id="myCart" href="#"><img src="{{ asset('plantilla-ecommerce/themes/images/ico-cart.png') }}" alt="cart">
 			3 productos en tu carrito<span class="badge badge-warning pull-right">$155.00</span></a>
 		</div>
 		@endauth
